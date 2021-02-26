4d7c97e0acab8ac11a003c010534edbb814109af
Leonardo <leo_narita@hotmail.com>
2021-02-25 21:26:17 -0300

    Testes, email, endpoints

---
+++ api/src/__tests__/_First.test.ts
@@ -0,0 +1,10 @@
+describe("First", () => {
+
+    it("deve ser possível 2 números", () => {
+        expect(2 + 2).toBe(4)
+    })
+
+    it("deve ser possível 2 números", () => {
+        expect(2 + 2).not.toBe(5)
+    })
+})
--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express'
+import { SendMailController } from './controllers/SendMailController'
+import { SurveysController } from './controllers/SurveysController'
 import { UserController } from './controllers/UsersController'
 
 const router = Router()
 
 const userController = new UserController()
+const surveyController = new SurveysController()
+const sendMailController = new SendMailController()
 
 /*
     GET    : Pegar
@@ -14,5 +18,10 @@ const userController = new UserController()
 */
 
 router.post("/users", userController.create)
+
+router.post("/surveys", surveyController.create)
+router.get("/surveys", surveyController.show)
+
+router.post("/sendMail", sendMailController.execute)
 
 export {router}
